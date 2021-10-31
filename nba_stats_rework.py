236cc7c34a43eabe88b38742ed1cba2bf61df9da
Terminathorns <nadersyed5@gmail.com>
2021-10-31 09:27:39 -0500

    Fixed Advanced and Shooting Stats formatting issues

--- nba_stats_rework.py
+++ nba_stats_rework.py
@@ -18,9 +18,144 @@ class nbastats:
 
     def __init__ (self,year):
         self.year = year
+
+    def team_per_game(self):
+        url = "https://www.basketball-reference.com/leagues/NBA_{}.html".format(self.year)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        tables = []
+        header = []
+        ptag = soup.findAll('p')[2]
+        champ = ptag.find('a').contents[0]
+        div = soup.find('div',{'id':'div_per_game-team'})
+        table = div.find('table',{'id':'per_game-team'})
+        
+        for tr in table.find_all('thead'):
+            for th in tr.find_all('th'):
+                column = th.text
+                header.append(column)
+                
+        for tr in table.find_all('tbody'):
+            for td in tr.find_all('td'):
+                row = td.text
+                tables.append(row)
+
+        df_template = []
+        for i in range(0,30):
+            df_template.append(list(tables[24*i:(24*i)+24]))
+
+        header.remove('Rk')
+        df = pd.DataFrame(df_template,columns=header)
+        df['Team'] = df['Team'].map(lambda x:x.rstrip('*'))
+        champ_check_list = []
+
+        for i in df['Team']:
+            if i == champ:
+                champ_check = True
+            else:
+                champ_check = False
+
+            champ_check_list.append(champ_check)
+
+        df['Champ?'] = champ_check_list
+
+        #print("table_container is_setup current" in r.text)
+        #print("div_per_poss-team" in r.text)
+        return df
+
+    def opp_per_game(self):
+        url = "https://www.basketball-reference.com/leagues/NBA_{}.html".format(self.year)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        tables = []
+        header = []
+        ptag = soup.findAll('p')[2]
+        champ = ptag.find('a').contents[0]
+        div = soup.find('div',{'id':'div_per_game-opponent'})
+        table = div.find('table',{'id':'per_game-opponent'})
+        
+        for tr in table.find_all('thead'):
+            for th in tr.find_all('th'):
+                column = th.text
+                header.append(column)
+                
+        for tr in table.find_all('tbody'):
+            for td in tr.find_all('td'):
+                row = td.text
+                tables.append(row)
+
+        df_template = []
+        for i in range(0,30):
+            df_template.append(list(tables[24*i:(24*i)+24]))
+
+        header.remove('Rk')
+        df = pd.DataFrame(df_template,columns=header)
+        df['Team'] = df['Team'].map(lambda x:x.rstrip('*'))
+        champ_check_list = []
+
+        for i in df['Team']:
+            if i == champ:
+                champ_check = True
+            else:
+                champ_check = False
+
+            champ_check_list.append(champ_check)
+
+        df['Champ?'] = champ_check_list
+
+        #print("table_container is_setup current" in r.text)
+        #print("div_per_poss-team" in r.text)
+        return df
+
+
+
+    def team_per_100(self):
+        url = "https://www.basketball-reference.com/leagues/NBA_{}.html".format(self.year)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        tables = []
+        header = []
+        ptag = soup.findAll('p')[2]
+        champ = ptag.find('a').contents[0]
+        div = soup.find('div',{'id':'div_per_poss-team'})
+        table = div.find('table',{'id':'per_poss-team'})
+        
+        for tr in table.find_all('thead'):
+            for th in tr.find_all('th'):
+                column = th.text
+                header.append(column)
+                
+        for tr in table.find_all('tbody'):
+            for td in tr.find_all('td'):
+                row = td.text
+                tables.append(row)
+
+        df_template = []
+        for i in range(0,30):
+            df_template.append(list(tables[24*i:(24*i)+24]))
+
+
+        header.remove('Rk')
+        df = pd.DataFrame(df_template,columns=header)
+        df['Team'] = df['Team'].map(lambda x:x.rstrip('*'))
+        champ_check_list = []
+
+        for i in df['Team']:
+            if i == champ:
+                champ_check = True
+            else:
+                champ_check = False
+
+            champ_check_list.append(champ_check)
+
+        df['Champ?'] = champ_check_list
+
+        #print("table_container is_setup current" in r.text)
+        #print("div_per_poss-team" in r.text)
+        return df
     
 
-    def team_opp100(self):
+    def opp_per_100(self):
         #Best defensive team each season (by DRtg), required table is Per 100 Poss Stats, Opponent Tab
         url = "https://www.basketball-reference.com/leagues/NBA_{}.html".format(self.year)
         r = requests.get(url)
@@ -49,43 +184,22 @@ class nbastats:
         header.remove('Rk')
         df = pd.DataFrame(df_template,columns=header)
         df['Team'] = df['Team'].map(lambda x:x.rstrip('*'))
+        champ_check_list = []
+
+        for i in df['Team']:
+            if i == champ:
+                champ_check = True
+            else:
+                champ_check = False
+
+            champ_check_list.append(champ_check)
+
+        df['Champ?'] = champ_check_list
 
         #print("table_container is_setup current" in r.text)
         #print("div_per_poss-team" in r.text)
         return df
 
-    def team_100(self):
-        url = "https://www.basketball-reference.com/leagues/NBA_{}.html".format(self.year)
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        tables = []
-        header = []
-        ptag = soup.findAll('p')[2]
-        champ = ptag.find('a').contents[0]
-        div = soup.find('div',{'id':'div_per_poss-team'})
-        table = div.find('table',{'id':'per_poss-team'})
-        
-        for tr in table.find_all('thead'):
-            for th in tr.find_all('th'):
-                column = th.text
-                header.append(column)
-                
-        for tr in table.find_all('tbody'):
-            for td in tr.find_all('td'):
-                row = td.text
-                tables.append(row)
-
-        df_template = []
-        for i in range(0,30):
-            df_template.append(list(tables[24*i:(24*i)+24]))
-
-        header.remove('Rk')
-        df = pd.DataFrame(df_template,columns=header)
-        df['Team'] = df['Team'].map(lambda x:x.rstrip('*'))
-
-        #print("table_container is_setup current" in r.text)
-        #print("div_per_poss-team" in r.text)
-        return df
 
     def team_advanced(self):
         url = "https://www.basketball-reference.com/leagues/NBA_{}.html".format(self.year)
@@ -105,21 +219,103 @@ class nbastats:
         header = [i for i in header if i not in ('','Rk','Offense Four Factors','Defense Four Factors','\xa0')]
                 
         for tr in table.find_all('tbody'):
-            for td in tr.find_all('td'):
-                row = td.text
-                tables.append(row)
-                tables = [val for val in tables if val not in ('','.')]
+            for td in tr.find_all('td',{'data-stat':'DUMMY'}):
+                #row = td(text=True,dummy=False)
+                #tables.append(''.join(row) if row else 'blank')
+                #tables = [val for val in tables if val not in ('.','')]
+                td.decompose()
 
+            for td in tr.find_all('td'):
+                row = td(text=True)
+                if row:
+                    tables.append(''.join(row))
+                else:
+                    tables.append('N/A')
+                tables = [val for val in tables if val not in ('.','')]
+        
         df_template = []
         for i in range(0,30):
             df_template.append(list(tables[27*i:(27*i)+27]))
         df = pd.DataFrame(df_template,columns=header)
         df['Team'] = df['Team'].map(lambda x:x.rstrip('*'))
+        champ_check_list = []
+
+        for i in df['Team']:
+            if i == champ:
+                champ_check = True
+            else:
+                champ_check = False
+
+            champ_check_list.append(champ_check)
+
+        df['Champ?'] = champ_check_list
 
         #print("table_container is_setup current" in r.text)
         #print("div_per_poss-team" in r.text)
         return df
+        
 
-print("Team Stats Per 100: \n",nbastats(2014).team_100(),"\n")
-print("Opponent Stats Per 100: \n",nbastats(2014).team_opp100(),"\n")
-print("Team Advanced Stats: \n",nbastats(2014).team_advanced(),"\n")
+    def team_shooting(self):
+        url = "https://www.basketball-reference.com/leagues/NBA_{}.html".format(self.year)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        tables = []
+        header = []
+        ptag = soup.findAll('p')[2]
+        champ = ptag.find('a').contents[0]
+        div = soup.find('div',{'id':'div_shooting-team'})
+        table = div.find('table',{'id':'shooting-team'})
+        
+        for tr in table.find_all('thead'):
+            for th in tr.find_all('th'):
+                column = th.text
+                header.append(column)
+        header = [i for i in header if i not in ('','\xa0','% of FGA by Distance','FG% by Distance','% of FG Ast\'d','Dunks','Layups','Corner','Heaves','Rk')]
+                
+        for tr in table.find_all('tbody'):
+            for td in tr.find_all('td',{'data-stat':'DUMMY'}):
+                #row = td(text=True,dummy=False)
+                #tables.append(''.join(row) if row else 'blank')
+                #tables = [val for val in tables if val not in ('.','')]
+                td.decompose()
+
+            for td in tr.find_all('td'):
+                row = td(text=True)
+                if row:
+                    tables.append(''.join(row))
+                else:
+                    tables.append('Not reported')
+                tables = [val for val in tables if val not in ('.','')]
+
+        df_template = []
+        for i in range(0,30):
+            df_template.append(list(tables[27*i:(27*i)+27]))
+    
+        df = pd.DataFrame(df_template,columns=header)
+        df['Team'] = df['Team'].map(lambda x:x.rstrip('*'))
+        champ_check_list = []
+
+        for i in df['Team']:
+            if i == champ:
+                champ_check = True
+            else:
+                champ_check = False
+
+            champ_check_list.append(champ_check)
+
+        df['Champ?'] = champ_check_list
+
+        #print("table_container is_setup current" in r.text)
+        #print("div_per_poss-team" in r.text)
+        
+        return df
+
+
+'''
+print("Team Stats Per Game: \n", nbastats(2021).team_per_game(),"\n")
+print("Opponent Stats Per Game: \n", nbastats(2021).opp_per_game(),"\n")
+
+print("Team Stats Per 100: \n",nbastats(2021).team_per_100(),"\n")
+print("Opponent Stats Per 100: \n",nbastats(2021).opp_per_100(),"\n")
+'''
+print("Team Advanced Stats: \n",nbastats(2015).team_shooting(),"\n")
