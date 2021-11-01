337e5f6db6d158b8f68fd29afeac4928ab1ddf0a
Terminathorns <nadersyed5@gmail.com>
2021-11-01 02:40:03 -0500

    File writer first build complete

--- nba_classifier.py
+++ nba_classifier.py
@@ -1,1 +1,31 @@
-import nba_stats_rework
+import nba_stats_rework as nsr
+import os
+
+directory = "/Users/macowner/Documents/Hello/NBA_Predictor/Predictor_csv_files/"
+start, end = int(input("Type in a start year for the search: ")), int(input("Type in an end year: "))
+years = range(start,end+1)
+tpg = []
+opp_per_game = []
+team_per_100 = []
+opp_per_game = []
+team_advanced = []
+team_shooting = []
+
+for i in years:
+    tpg.append(nsr.nbastats(i).team_per_game())
+    with open(directory + "{}_team_per_game.txt".format(i), "w") as f1:
+        f1.write(tpg[i-start].to_csv())
+'''
+for i in years:
+    opg = nsr.nbastats(i).opp_per_game()
+for i in years:
+    tp100 = nsr.nbastats(i).team_per_100()
+for i in years:
+    op100 = nsr.nbastats(i).opp_per_100()
+for i in years:
+    advanced = nsr.nbastats(i).team_advanced()
+for i in years:
+    tsh = nsr.nbastats(i).team_shooting()
+for i in years:
+    osh = nsr.nbastats(i).opp_shooting()
+'''
--- nba_stats_rework.py
+++ nba_stats_rework.py
@@ -31,9 +31,10 @@ class nbastats:
                 tables.append(row)
 
         df_template = []
-        for i in range(0,30):
+        
+        for i in range(0,len(table.find_all('tr'))-2):
             df_template.append(list(tables[24*i:(24*i)+24]))
-
+    
         header.remove('Rk')
         df = pd.DataFrame(df_template,columns=header)
         df['Team'] = df['Team'].map(lambda x:x.rstrip('*'))
@@ -51,6 +52,7 @@ class nbastats:
 
         #print("table_container is_setup current" in r.text)
         #print("div_per_poss-team" in r.text)
+    
         return df
 
     def opp_per_game(self):
@@ -75,7 +77,7 @@ class nbastats:
                 tables.append(row)
 
         df_template = []
-        for i in range(0,30):
+        for i in range(0,len(table.find_all('tr'))-2):
             df_template.append(list(tables[24*i:(24*i)+24]))
 
         header.remove('Rk')
@@ -121,7 +123,7 @@ class nbastats:
                 tables.append(row)
 
         df_template = []
-        for i in range(0,30):
+        for i in range(0,len(table.find_all('tr'))-2):
             df_template.append(list(tables[24*i:(24*i)+24]))
 
 
@@ -168,7 +170,7 @@ class nbastats:
                 tables.append(row)
 
         df_template = []
-        for i in range(0,30):
+        for i in range(0,len(table.find_all('tr'))-2):
             df_template.append(list(tables[24*i:(24*i)+24]))
 
         header.remove('Rk')
@@ -224,8 +226,10 @@ class nbastats:
                 tables = [val for val in tables if val not in ('.','')]
         
         df_template = []
-        for i in range(0,30):
+        
+        for i in range(0,len(table.find_all('tr'))-3):
             df_template.append(list(tables[27*i:(27*i)+27]))
+    
         df = pd.DataFrame(df_template,columns=header)
         df['Team'] = df['Team'].map(lambda x:x.rstrip('*'))
         champ_check_list = []
@@ -242,6 +246,7 @@ class nbastats:
 
         #print("table_container is_setup current" in r.text)
         #print("div_per_poss-team" in r.text)
+    
         return df
         
 
@@ -278,7 +283,7 @@ class nbastats:
                 tables = [val for val in tables if val not in ('.','')]
 
         df_template = []
-        for i in range(0,30):
+        for i in range(0,len(table.find_all('tr'))-2):
             df_template.append(list(tables[27*i:(27*i)+27]))
     
         df = pd.DataFrame(df_template,columns=header)
@@ -299,3 +304,95 @@ class nbastats:
         #print("div_per_poss-team" in r.text)
         
         return df
+
+
+
+    def opp_shooting(self):
+        url = "https://www.basketball-reference.com/leagues/NBA_{}.html".format(self.year)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        tables = []
+        header = []
+        ptag = soup.findAll('p')[2]
+        champ = ptag.find('a').contents[0]
+        div = soup.find('div',{'id':'div_shooting-opponent'})
+        table = div.find('table',{'id':'shooting-opponent'})
+        
+        for tr in table.find_all('thead'):
+            for th in tr.find_all('th'):
+                column = th.text
+                header.append(column)
+        header = [i for i in header if i not in ('','\xa0','% of FGA by Distance','FG% by Distance','% of FG Ast\'d','Dunks','Layups','Corner','Heaves','Rk')]
+                
+        for tr in table.find_all('tbody'):
+            for td in tr.find_all('td',{'data-stat':'DUMMY'}):
+                #row = td(text=True,dummy=False)
+                #tables.append(''.join(row) if row else 'blank')
+                #tables = [val for val in tables if val not in ('.','')]
+                td.decompose()
+
+            for td in tr.find_all('td'):
+                row = td(text=True)
+                if row:
+                    tables.append(''.join(row))
+                else:
+                    tables.append('Not reported')
+                tables = [val for val in tables if val not in ('.','')]
+
+        df_template = []
+        for i in range(0,len(table.find_all('tr'))-2):
+            df_template.append(list(tables[27*i:(27*i)+27]))
+    
+        df = pd.DataFrame(df_template,columns=header)
+        df['Team'] = df['Team'].map(lambda x:x.rstrip('*'))
+        champ_check_list = []
+
+        for i in df['Team']:
+            if i == champ:
+                champ_check = True
+            else:
+                champ_check = False
+
+            champ_check_list.append(champ_check)
+
+        df['Champ?'] = champ_check_list
+
+        #print("table_container is_setup current" in r.text)
+        #print("div_per_poss-team" in r.text)
+        return df
+
+directory = "/Users/macowner/Documents/Predictor_csv_files/"
+start, end = int(input("Type in a start year for the search: ")), int(input("Type in an end year: "))
+years = range(start,end+1)
+team_per_game_list = []
+opp_per_game_list = []
+team_per_100_list = []
+opp_per_100_list = []
+team_advanced_list = []
+
+for i in years:
+    team_per_game_list.append(nbastats(i).team_per_game())
+    with open(directory + "{}_team_per_game.txt".format(i), "w") as f1:
+        f1.write(team_per_game_list[i-start].to_csv())
+
+    opp_per_game_list.append(nbastats(i).opp_per_game())
+    with open(directory + "{}_opp_per_game.txt".format(i), "w") as f1:
+        f1.write(opp_per_game_list[i-start].to_csv())
+
+    team_per_100_list.append(nbastats(i).team_per_100())
+    with open(directory + "{}_team_per_100.txt".format(i), "w") as f1:
+        f1.write(team_per_100_list[i-start].to_csv())
+
+    opp_per_100_list.append(nbastats(i).opp_per_100())
+    with open(directory + "{}_opp_per_100.txt".format(i), "w") as f1:
+        f1.write(opp_per_100_list[i-start].to_csv())
+
+    team_advanced_list.append(nbastats(i).team_advanced())
+    with open(directory + "{}_team_advanced.txt".format(i), "w") as f1:
+        f1.write(team_advanced_list[i-start].to_csv())
+
+    '''
+    team_shooting_list.append(nbastats(i).team_shooting())
+    opp_shooting_list.append(nbastats(i).opp_shooting())
+    '''
+
