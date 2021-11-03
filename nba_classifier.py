ee7df3fd10d544c5ffc3bec7059a8628f3407cbf
Terminathorns <nadersyed5@gmail.com>
2021-11-02 22:29:40 -0500

    Put classifier into a function

--- nba_classifier.py
+++ nba_classifier.py
@@ -2,7 +2,6 @@ from imblearn.ensemble import BalancedRandomForestClassifier
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
-import nba_stats_rework as nsr
 import numpy as np
 from sklearn.metrics import accuracy_score
 
@@ -10,16 +9,20 @@ directory = "/Users/macowner/Documents/Predictor_csv_files/"
 
 
 def classifier(start,end):
-    tp100 = []
-    op100 = []
-    tadv = []
     cdf = pd.read_csv(directory + "{}_to_{}_team_stats.txt".format(start,end))
-    return cdf
+    features = cdf.drop(['G','MP','Arena','Attend.','Attend./G'],1)
+    label = cdf['Champ?']
+    ohe = preprocessing.OneHotEncoder()
+    features = ohe.fit_transform(features)
+    np.set_printoptions(threshold=np.inf)
 
-testdf = classifier(1990,2021)
+    X_train, X_test, y_train, y_test = train_test_split(features,label,test_size=0.25,random_state=20,stratify=label)
+    brfc = BalancedRandomForestClassifier()
+    model = brfc.fit(X_train,y_train)
+    preds = model.predict(X_test)
+    return preds, ohe.inverse_transform(X_test),"Model accuracy: " + str(accuracy_score(y_test,preds)*100) + "%"
 
-features = testdf.drop(['G','MP','Arena','Attend.','Attend./G'],1)
-label = testdf['Champ?']
+print(classifier(1990,2021))
 
 '''
 testdf_new = classifier(2022,2022)
@@ -27,28 +30,13 @@ features_new = testdf_new.drop(['G','MP','Arena','Attend.','Attend./G'],1)
 label_new = testdf_new['Champ?']
 '''
 
-ohe = preprocessing.OneHotEncoder()
-features = ohe.fit_transform(features)
-np.set_printoptions(threshold=np.inf)
 
-X_train, X_test, y_train, y_test = train_test_split(features,label,test_size=0.45,random_state=20,stratify=label)
-brfc = BalancedRandomForestClassifier()
-model = brfc.fit(X_train,y_train)
-preds = model.predict(X_test)
 
 '''
 features_new = ohe.fit_transform(features_new)
 np.set_printoptions(threshold=np.inf)
 model.predict(features_new)
 '''
-
-print(ohe.inverse_transform(X_test))
-print(X_test)
-print(preds)
-print(np.shape(features))
-print("Model accuracy: " + str(accuracy_score(y_test,preds)*100) + "%")
-
-
 
         
 
